3d827fba1b29f33827793aa40fd9ca29fbbc901b
natanri <riosjhonatan44@gmail.com>
2022-01-29 20:40:04 -0500

    Initial commit

---
+++ src/rios/Main.java
@@ -0,0 +1,10 @@
+package rios;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Jhonatan");
+
+    }
+}
